8d502dc4bc13847e01ae71ce83ee7f1c67eba742
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2025-04-23 11:33:14 -0300

    fixed minor bug for content

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clarity',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     # Add other metadata like author, description, etc. later
 )
